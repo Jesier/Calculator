d34643f35357682e1edee8da5120cb3338d920bf
Jesier <jesiersantiago@gmail.com>
2022-10-07 10:31:13 -0500

    push

---
+++ main.js
@@ -0,0 +1,32 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+
+        return sum
+}
+
+const subtract = (firstNumber, secondNumber) => {
+    const sum = firstNumber - secondNumber
+
+        return sum
+}
+
+const multiply = (firstNumber, secondNumber) => {
+    const sum = firstNumber * secondNumber
+
+        return sum
+}
+
+const square = (firstNumber) => {
+    const sum = firstNumber * firstNumber
+        return sum
+}
+
+console.log(square(9))
+let squared = 81
+console.log(square(squared))
+
+
+/*there are 60 legs on the farm and i need to find how many animals are on the farm
+  there are 3/4 have two legs and 1/4 have four legs
+  im a software developer not a mathmetician*/
+  
